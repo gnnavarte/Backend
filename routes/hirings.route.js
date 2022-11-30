82c41081f7ea355f8e66821a9e595a9b32a73b9a
Gonzalo Navarte <gnavarte@uade.edu.ar>
2022-11-30 14:19:51 -0300

    Cmabiardo

--- routes/hirings.route.js
+++ routes/hirings.route.js
@@ -9,7 +9,7 @@ router.get('/test', function(req, res) {
 router.post('/',Authorization , HiringController.createHiring)
 router.get('/', HiringController.getHirings)
 router.get('/:id', HiringController.getHiringsById)
-router.put('/approve',Authorization , HiringController.approveHiring)
+router.put('/approve/:id',Authorization , HiringController.approveHiring)
 router.delete('/:id',Authorization , HiringController.removeHiring)
 
 module.exports = router;
